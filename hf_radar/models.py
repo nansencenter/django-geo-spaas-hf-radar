9169744208f4ff9ce85d768254a2a19e75cb28c4
Artem Moiseev <korvinos@bk.ru>
2020-07-21 07:51:25 +0000

    style cleaning

--- hf_radar/models.py
+++ hf_radar/models.py
@@ -1,12 +1,9 @@
 from __future__ import unicode_literals
-
-from django.db import models
 from geospaas.catalog.models import Dataset as CatalogDataset
 from hf_radar.managers import HFRDatasetManager
 
+
 class HFRDataset(CatalogDataset):
-
     objects = HFRDatasetManager()
-
     class Meta:
         proxy = True
